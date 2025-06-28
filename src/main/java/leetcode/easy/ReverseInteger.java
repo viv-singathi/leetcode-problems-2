999a59057419716270d0562ab2f1f4fbaebbf576
Vivek Singathi <viv.singathi@gmail.com>
2025-06-28 10:56:56 -0400

    Initial version

---
+++ src/main/java/leetcode/easy/ReverseInteger.java
@@ -0,0 +1,33 @@
+package leetcode.easy;
+
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+        System.out.println(reverse(-123));
+
+    }
+
+    public static int reverse(int x) {
+        if (x == 0) {return 0;}
+
+        long n = x % 10;
+        x = x/10;
+
+        while (x != 0){
+            int m = x % 10;
+            if (m != 0 || n != 0){
+                n *= 10;
+                n += m;
+            }
+            x = x/10;
+        }
+
+        if (Math.abs(n) > (1 << 31) - 1){
+            return 0;
+        }
+
+
+        return (int) n;
+    }
+}
