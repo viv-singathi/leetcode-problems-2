242434b2b83b69dc61b861f92eef9edc8bbd791e
Vivek Singathi <viv.singathi@gmail.com>
2025-07-03 11:23:09 -0400

    problem

---
+++ src/main/java/leetcode/medium/SumRootToLeafNumbers.java
@@ -0,0 +1,51 @@
+package leetcode.medium;
+
+public class SumRootToLeafNumbers {
+
+    public static  void main(String args[]){
+        TreeNode l = new TreeNode(2);
+        TreeNode r = new TreeNode(3);
+        TreeNode root = new TreeNode(1, l, r);
+        System.out.println(sumNumbers(root));
+        //output: 25
+    }
+
+    static int s = 0;
+    public static int sumNumbers(TreeNode root) {
+
+        sumTree(root, 0);
+
+        int sum = s;
+        s = 0;
+
+        return sum;
+    }
+
+    public static void sumTree(TreeNode t, int currentSum){
+        currentSum = currentSum * 10 + t.val;
+
+        if (t.left != null){
+            sumTree(t.left, currentSum);
+        }
+        if (t.right != null){
+            sumTree(t.right, currentSum);
+        }
+
+        if (t.left == null && t.right == null){
+            s += currentSum;
+        }
+    }
+
+    public static class TreeNode {
+       int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode() {}
+         TreeNode(int val) { this.val = val; }
+         TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+         }
+     }
+}
